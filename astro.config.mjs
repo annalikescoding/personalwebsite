5f065beb90b3ee53ab6bc9e509a4ced34006e326
annalikescoding <annalac2139@gmail.com>
2026-07-14 09:54:25 -0700

    added config

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,6 +4,8 @@ import tailwindcss from "@tailwindcss/vite";
 
 // https://astro.build/config
 export default defineConfig({
+  site: "https://annalikescoding.github.io",
+  base: "/personalwebsite/",
   vite: {
     plugins: [tailwindcss()],
   },
